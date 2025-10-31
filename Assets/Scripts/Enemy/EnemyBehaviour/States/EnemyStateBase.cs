cde3c5021dd62a1b1b60b1029eeef095e2199685
Mussk <sanja.vin1999@gmail.com>
2025-10-31 02:41:01 +0100

    fix bug with needsExitTime
    added a base constructor to EnemyStateBase to pass readonly variable needsExitTime

--- Assets/Scripts/Enemy/EnemyBehaviour/States/EnemyStateBase.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour/States/EnemyStateBase.cs
@@ -1,70 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityHFSM;
-using UnityEngine.AI;
 using System;
-using Enemy;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityHFSM;
 
-public abstract class EnemyStateBase : State<EnemyState, StateEvent>
+namespace Enemy.EnemyBehaviour.States
 {
-    protected readonly BaseEnemyController Enemy;
-    protected readonly NavMeshAgent Agent;
-    protected readonly Animator Animator;
-    protected bool RequestedExit;
-    protected float ExitTime;
-
-    protected readonly Action<State<EnemyState, StateEvent>> onEnter;
-    protected readonly Action<State<EnemyState, StateEvent>> onLogic;
-    protected readonly Action<State<EnemyState, StateEvent>> onExit;
-    protected readonly Func<State<EnemyState, StateEvent>, bool> canExit;
-
-    public EnemyStateBase(bool needsExitTime,
-        BaseEnemyController Enemy,
-        float ExitTime = 0.1f,
-        Action<State<EnemyState, StateEvent>> onEnter = null,
-        Action<State<EnemyState, StateEvent>> onLogic = null,
-        Action<State<EnemyState, StateEvent>> onExit = null,
-        Func<State<EnemyState, StateEvent>, bool> canExit = null)
+    public abstract class EnemyStateBase : State<EnemyState, StateEvent>
     {
-        this.Enemy = Enemy;
-        this.onEnter = onEnter;
-        this.onLogic = onLogic;
-        this.onExit = onExit;
-        this.canExit = canExit;
-        this.ExitTime = ExitTime;
-        //this.needsExitTime = needsExitTime;
-        Agent = Enemy.GetComponent<NavMeshAgent>();
-        Animator = Enemy.GetComponent<Animator>();
-    }
+        protected readonly BaseEnemyController Enemy;
+        protected readonly NavMeshAgent Agent;
+        protected readonly Animator Animator;
+        protected bool RequestedExit;
+        protected float ExitTime;
+       
 
-    public override void OnEnter()
-    {
-        base.OnEnter();
-        RequestedExit = false;
-        onEnter?.Invoke(this);
-    }
-
-    public override void OnLogic()
-    {
-        base.OnLogic();
-        if (RequestedExit && timer.Elapsed >= ExitTime)
+        protected readonly Action<State<EnemyState, StateEvent>> onEnter;
+        protected readonly Action<State<EnemyState, StateEvent>> onLogic;
+        protected readonly Action<State<EnemyState, StateEvent>> onExit;
+        protected readonly Func<State<EnemyState, StateEvent>, bool> canExit;
+    
+        public EnemyStateBase(bool needsExitTime,
+            BaseEnemyController enemy,
+            float exitTime = 0.1f,
+            Action<State<EnemyState, StateEvent>> onEnter = null,
+            Action<State<EnemyState, StateEvent>> onLogic = null,
+            Action<State<EnemyState, StateEvent>> onExit = null,
+            Func<State<EnemyState, StateEvent>, bool> canExit = null) : base(onEnter, onLogic, onExit, canExit, needsExitTime)
         {
-            fsm.StateCanExit();
-           
-        }
-    }
-
-    public override void OnExitRequest()
-    {
-        if (!needsExitTime || canExit != null && canExit(this))
-        {
-            fsm.StateCanExit();
-        }
-        else
-        {
+            this.Enemy = enemy;
+            this.onEnter = onEnter;
+            this.onLogic = onLogic;
+            this.onExit = onExit;
+            this.canExit = canExit;
+            this.ExitTime = exitTime;
             
-            RequestedExit = true;
+            Agent = Enemy.GetComponent<NavMeshAgent>();
+            Animator = Enemy.GetComponentInChildren<Animator>();
         }
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            RequestedExit = false;
+            onEnter?.Invoke(this);
+        }
+
+        public override void OnLogic()
+        {
+            base.OnLogic();
+        
+            if (RequestedExit && timer.Elapsed >= ExitTime)
+            {
+                fsm.StateCanExit();
+           
+            }
+        }
+        public override void OnExitRequest()
+        {
+            if (!needsExitTime || canExit != null && canExit(this))
+            {
+                fsm.StateCanExit();
+            }
+            else
+            {
+            
+                RequestedExit = true;
+            }
+        }
+    
+   
     }
 }
